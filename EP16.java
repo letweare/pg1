d97594a09ad248ff30cea69b1e219a649346a56a
letweare <53572040+letweare@users.noreply.github.com>
2019-09-22 16:34:10 +0800

    Add files via upload

---
+++ EP16.java
@@ -0,0 +1,14 @@
+class Animal {
+	void shout () {
+		System.out.println ("¶¯Îï½Ð£¡");
+	}
+} 
+
+public class EP16{
+	
+	public static void main (String [] args ){
+		
+		Animal animal = new Animal ();
+		System.out.println (animal.toString());
+	}
+}
---
+++ EP17.java
@@ -0,0 +1,13 @@
+class Animal {
+	
+	public String toString (){
+		return "I am an animal ";
+	}
+}
+  
+public class  EP17{
+	public static void main (String [] args){
+		Animal animal= new Animal ();
+		System.out.println(animal.toString());
+	}
+}
