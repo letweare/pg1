0da04c885454300a6f8e18b2ccf10b48b6143a7d
letweare <53572040+letweare@users.noreply.github.com>
2019-09-22 16:28:50 +0800

    Add files via upload

---
+++ EP10.java
@@ -0,0 +1,16 @@
+abstract class Animal {
+	abstract void shout();
+} 
+
+class Dog extends Animal {
+	 void shout(){
+		 System.out.println("wangwang");
+	 }
+} 
+
+public class EP10{
+	public static void main (String [] args){
+		Dog dog=new Dog();
+		dog.shout();
+	}
+}
---
+++ EP11.java
@@ -0,0 +1,21 @@
+interface Animal{
+	int id=1;
+	void breathe ();
+	void run();
+}  
+ class Dog  implements Animal {
+	 public void breathe (){
+		 System.out.println ("dog ÕýÔÚºôÎü");
+	 }
+	 public void run (){
+		 System.out.println ("dog is running ");
+	 }
+ } 
+ 
+ public class EP11{
+	 public static void main (String [] args){
+		 Dog dog= new Dog();
+		 dog.breathe();
+		 dog.run();
+	 }
+ }
