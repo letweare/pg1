fab9f9509de88394e580272e8fcef359be5aeee2
letweare <53572040+letweare@users.noreply.github.com>
2019-09-28 21:30:30 +0800

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+import Course.*;
+import Teacher.*;
+import student.*;
+public class MainClass {
+        public static void main (String [] args)
+        {
+        	Course cs=new Course ();
+        	Teacher te= new Teacher ();
+        	Student st = new Student();
+        }
+}
